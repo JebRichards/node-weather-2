74c8aac81f7857d31039810108625178a580e2ce
Becca Cubitt <beccacubitt@Beccas-MBP.lan>
2020-01-14 09:51:48 +0000

    Changed footer and weather result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/f87f08a641cd60b3cc450f186a60e983/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/f87f08a641cd60b3cc450f186a60e983/' + latitude + ',' + longitude + "?units=si"
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress Celcius and there is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
